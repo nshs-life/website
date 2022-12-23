294644745cae073b849b498ad420ebc1826e9849
Max <max.b.raffel@gmail.com>
2022-12-23 00:22:37 -0500

    cleaned up html typos + work on scrolling button

--- buttons.js
+++ buttons.js
@@ -1,5 +1,14 @@
-const slideDown = () => {
+//loop through each button
+$('.next-section').each(function() {
+    $(this).on('click', function () {
+        //find the button's parent
+        var nextSection = $(this).parent().next();
+        $(nextSection).scrollIntoView({ behavior: smooth });
+    });
+});
 
+const slideDown = (target) => {
+    
 }
 
 const slideUp = () => {
